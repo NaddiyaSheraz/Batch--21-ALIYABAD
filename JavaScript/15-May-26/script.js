00e7fc3c02ba7be98a63f48426a97f0cc8a94c61
NaddiyaSheraz <naddiyasheraz18@gmail.com>
2026-05-20 10:48:11 +0500

    fix broken git reference

---
+++ JavaScript/15-May-26/script.js
@@ -0,0 +1,108 @@
+// prompt
+//var username = prompt("Enter your name.","XYZ");
+
+//if ch#10
+// condition
+// syntax
+
+// if (condition){
+//     code
+// }
+
+// var name = "Hira";
+
+// if (name == "hira"){
+//     alert("Welcome" + name)
+// }
+
+// var userName = prompt("Enter your name");
+
+// if (userName == "Aleena"){
+//     document.write("hello Aleena");
+// }
+
+// var Education = prompt("Enter you last Education");
+
+// if (Education === "matric"){
+//     document.write("you are eligible");
+// }
+
+// if (Education === "inter"){
+//     document.write("Your are not Eligible");
+// }
+
+// if (Education === "Master"){
+//     document.write("Your are not Eligible because you are highly Qualified");
+// }
+
+// var age = prompt("Enter your age");
+
+// if (age == 18){
+//     alert("you can vote");
+// }
+
+// //less then
+// if (age < 18){
+//     alert("you can also vote")
+// }
+
+// if (age > 18){
+//         alert("you are allowed to vote")
+// }
+
+
+// var password = 1234;
+
+// if (password === 1234){
+//     document.write("Welcome Admin");
+// }
+// prompt("Enter your passwrod");
+
+// var password = +prompt("Enter your passwrod");
+
+// if (password === 12345){
+//     document.write("<h1>" + "Welcome Admin" + "</h1>");
+
+
+// }
+
+// var num = prompt("Enter a number");
+// var num2 = 10;
+
+// if (num == num2){
+//     document.write("your Guess is write");
+// }
+
+// if (num != num2){
+//     document.write("your guess is wrong")
+// }
+
+// var sum = prompt("What is sum of 2 + 5;")
+// var score = 0;
+
+// if (sum == 7){
+//     document.write("Hurry! your Answer is write.<br>");
+//     score++;
+//     document.write(score);
+// }
+
+// var name = prompt("Enter your name");
+// if (name == "Admin"){
+//     document.write("hello")
+// }
+
+// if (name != "Admin"){
+//     document.write("Your are not allowed")
+// }
+
+var pass = prompt("Enter your name");
+if (pass === 1234){ //equal to  value ==
+    //strick equal to === value datatype
+    document.write("hello")
+}
+
+if (pass !== 1234){
+    document.write("Your are not allowed")
+}
+
+
