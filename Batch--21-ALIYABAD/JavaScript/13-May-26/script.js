888cc56bc0bb0447a71f12b7239cf9079d67515b
NaddiyaSheraz <naddiyasheraz18@gmail.com>
2026-09-15 19:33:40 +0500

    14-sep-26 promise

---
+++ Batch--21-ALIYABAD/JavaScript/13-May-26/script.js
@@ -0,0 +1,40 @@
+
+
+// document.write(num1 + num2 + num3);
+
+// (2 + 3) + 554 * 5 
+// var num1 = "2";
+// var num2 = "10";
+
+// document.write(num1 + num2);
+
+
+
+// var x = 10; //9 
+// var y = --x + 5;
+
+// alert(y);
+
+// var y = x++ + --x + xx;
+
+// var userName ="Hajra";
+// // document.write("Our First Your is" + " " + "<h2>" + userName + "</h2>" );
+
+// // document.write("Hajra Fatima")
+
+// alert("Thank you" + " " + userName + " " + "!");
+
+//prompt
+// alert("Enter you Name");
+// var userName = prompt("Enter you Name");
+// document.write("Our first your :" + " " + "<strong>" + userName + "</strong>");
+
+// var age = prompt("Enter  you age?")
+// // document.write(age);
+// // document.write(userName + " " + "is" + " " + age + " " + "<stong>" + "years old" + "</strong>")
+// var newAge = age * 2;
+// document.write(newAge);
+document.write("Hello\nWorld")
+console.log("Hello\nWorld");
+
+
